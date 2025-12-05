716963ae3a46515ac7571f64aec12ef8084f6347
Shashank <jsr.shashank@gmail.com>
2025-12-05 17:55:00 +0530

    first commit

---
+++ Frontend/src/components/Table.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "./Table.css";
+const Table = () => {
+  return (
+    <>
+      <div>STUDENT TABLE</div>
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>NAME</th>
+              <th>AGE</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Chandan</td>
+              <td>25</td>
+              <td>
+                <div>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Manya</td>
+              <td>20</td>
+              <td>
+                <div>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Sandeep</td>
+              <td>22</td>
+              <td>
+                <div>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Farooq</td>
+              <td>24</td>
+              <td>
+                <div>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Manish</td>
+              <td>23</td>
+              <td>
+                <div>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Table;
