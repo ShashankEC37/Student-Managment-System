e7d0edffe9d3688c5ffb38f8d1d9430f8cafac37
Shashank <jsr.shashank@gmail.com>
2025-12-08 16:41:06 +0530

    Add CORS and Mongoose support; update package dependencies

---
+++ Backend/connection/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+async function ConnectDB() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://shashank:Shashank1234@cluster0.eyllfv3.mongodb.net/?appName=Cluster0"
+    );
+    console.log("Connected Mongodb");
+  } catch (error) {
+    console.log("Error Connecting to Mongodb", error);
+  }
+}
+
+module.exports = ConnectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const ConnectDB = require("./connection/db");
+app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
+
+ConnectDB();
 
 app.listen(PORT, () => {
   console.log("Application is running in PORT", PORT);
